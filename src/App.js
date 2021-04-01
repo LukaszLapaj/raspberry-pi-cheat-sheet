ecd88276470dd941590b398d25c35da52601c64a
Łukasz Łapaj <4143729+LukaszLapaj@users.noreply.github.com>
2021-04-01 12:09:18 +0200

    Add markdown viewing feature

--- src/App.js
+++ src/App.js
@@ -1,8 +1,32 @@
 import './App.css';
+import ReactMarkdown from 'react-markdown'
+import {useEffect, useState} from "react";
 
 function App() {
+    const [markdownText, setMarkdownText] = useState('');
+
+    const readmeUrl = "https://raw.githubusercontent.com/LukaszLapaj/raspberry-pi-cheat-sheet/master/README.md";
+    const tableOfContentsRegex = new RegExp("# Table of contents[\\s\\S]*?[\\n](?=#)");
+
+    useEffect(() => {
+        fetch(readmeUrl)
+            .then((response) => {
+                if (response.ok) return response.text();
+                else return Promise.reject("Didn't fetch text correctly");
+            })
+            .then((text) => {
+                let skippedTableOfContents = text.replace(tableOfContentsRegex, "");
+                setMarkdownText(skippedTableOfContents);
+            })
+            .catch((error) => console.error(error));
+    });
+
     return (
-        <></>
+        <div className="markdown-container">
+            <div className="markdown-container-inner">
+                <ReactMarkdown source={markdownText}/>
+            </div>
+        </div>
     );
 }
 
